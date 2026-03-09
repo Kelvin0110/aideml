89b783237c97d85e80ad2261228a72ee373d25f9
Kelvin0110 <kwtamai@connect.ust.hk>
2026-03-09 21:20:33 +0800

    feat: update Interpreter to use spawn context for multiprocessing queues

--- aide/interpreter.py
+++ aide/interpreter.py
@@ -15,6 +15,7 @@ import time
 import traceback
 from dataclasses import dataclass
 from multiprocessing import Process, Queue
+import multiprocessing
 from pathlib import Path
 
 import humanize
@@ -176,8 +177,9 @@ class Interpreter:
         # - result_outq: receive stdout/stderr from child
         # - event_outq: receive events from child (e.g. state:ready, state:finished)
         # trunk-ignore(mypy/var-annotated)
-        self.code_inq, self.result_outq, self.event_outq = Queue(), Queue(), Queue()
-        self.process = Process(
+        ctx = multiprocessing.get_context("spawn")
+        self.code_inq, self.result_outq, self.event_outq = ctx.Queue(), ctx.Queue(), ctx.Queue()
+        self.process = ctx.Process(
             target=self._run_session,
             args=(self.code_inq, self.result_outq, self.event_outq),
         )
